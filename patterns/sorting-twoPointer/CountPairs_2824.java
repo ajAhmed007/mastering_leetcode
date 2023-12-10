0012c9573683015eb968e93217b4c7198e003d44
ajAhmed007 <ahmedabdullahi9540@gmail.com>
2023-12-09 21:22:19 -0500

    LC 2824

---
+++ patterns/sorting-twoPointer/CountPairs_2824.java
@@ -0,0 +1,24 @@
+package patterns.sorting-twoPointer;
+
+public class CountPairs_2824 {
+    public int countPairs(List<Integer> nums, int target) {
+        // sort the numbers
+        Collections.sort(nums);
+
+        int count = 0;
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            if (nums.get(left) + nums.get(right) < target) {
+                count += right - left; // all nums between right to left
+                left++;
+            } else {
+                right--; // shrink the right
+            }
+        }
+        return count;
+    }
+    
+}
