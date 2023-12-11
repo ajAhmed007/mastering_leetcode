ee5ae0b51ef88c04a57beb9e449cd5e93daa1335
ajAhmed007 <ahmedabdullahi9540@gmail.com>
2023-12-10 19:09:11 -0500

    LC 1768

--- patterns/string_twoPointers/AlternativeMerge_1768.java
+++ patterns/string_twoPointers/AlternativeMerge_1768.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class AlternativeMerge_1768 {
     public static void main(String[] args) {
-        String word1 = "abc"
-        String word2 = "pqr"
+        String word1 = "abc";
+        String word2 = "pqr";
         System.out.println(mergeAlternately(word1, word2));
     }
     public static String mergeAlternately(String word1, String word2) {
